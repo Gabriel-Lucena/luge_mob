b36e0921fb90fd6c9bba4bf5910ee8e0b305489c
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2023-07-23 20:17:27 -0300

    Activity de login criada

---
+++ app/src/main/java/com/example/luge/api/Endpoint.kt
@@ -0,0 +1,4 @@
+package com.example.luge.api
+
+class Endpoint {
+}
---
+++ app/src/main/java/com/example/luge/util/Endpoint.kt
@@ -0,0 +1,12 @@
+package com.example.luge.util
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface Endpoint {
+
+    // Login
+
+}
